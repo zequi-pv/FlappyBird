679ac30481ea3432af94690772735be3895114ad
leo <leo99brizuela@gmail.com>
2023-11-10 01:45:28 -0300

    add gravity functions in bird.h

--- src/Bird.cpp
+++ src/Bird.cpp
@@ -25,31 +25,80 @@ void DrawBird(Bird bird)
 	DrawTexture(bird.texture, static_cast<int>(bird.pos.x), static_cast<int>(bird.pos.y), bird.color);
 }
 
+
 void MoveBird(Bird& bird)
 {
-	/*bird.pos.y += bird.speed * GetFrameTime();*/
-	/*if (bird.pos.y > 0|| bird.pos.y < GetScreenHeight() - 50)
-	{*/
-		if (IsKeyPressed(KEY_SPACE) && bird.pos.y > 0)
-		{
-			bird.pos.y -=500  * bird.speed * GetFrameTime();
-			
+    float gravity = 800.0f;  // Gravedad
+    float jumpAcceleration = -500.0f;  // Ajusta esta aceleración para controlar el salto
 
+    if (IsKeyPressed(KEY_SPACE) && !bird.isJumping)
+    {
+        bird.isJumping = true;
+        bird.speed = jumpAcceleration;
+    }
+   
+
+    if (bird.isJumping)
+    {
+        bird.pos.y += bird.speed * GetFrameTime();
+        bird.speed += gravity * GetFrameTime();
+
+        //// Cuando el pájaro alcanza su punto más bajo, deja de saltar
+        if (bird.pos.y >= GetScreenHeight() - bird.size.y)
+        {
+            
+        bird.isJumping = false;
+            bird.pos.y = GetScreenHeight() - bird.size.y;
+        }
+        
 
 #ifdef _DEBUG
-			cout << "y:" << bird.pos.y << endl;
+        cout << "y:" << bird.pos.y << endl;
 #endif
-		}
-		else if (bird.pos.y < GetScreenHeight() - 50)
-		{
-			bird.pos.y += 1 * bird.speed * GetFrameTime();
+    }
+    else
+    {
+       
+        // Aplica la gravedad al pájaro cuando no está saltando
+        bird.pos.y += gravity * GetFrameTime();
+
+        // Limita la posición del pájaro para que no se salga de la pantalla
+        if (bird.pos.y > GetScreenHeight() - bird.size.y)
+        {
+            bird.pos.y = GetScreenHeight() - bird.size.y;
+        }
 
 #ifdef _DEBUG
-			cout << "y:" << bird.pos.y << endl;
-#endif	
-		}	
-	/*}*/
-}
+        cout << "y:" << bird.pos.y << endl;
+#endif
+    }
+    }
+
+//void MoveBird(Bird& bird)
+//{
+//	/*bird.pos.y += bird.speed * GetFrameTime();*/
+//	/*if (bird.pos.y > 0|| bird.pos.y < GetScreenHeight() - 50)
+//	{*/
+//		if (IsKeyPressed(KEY_SPACE) && bird.pos.y > 0)
+//		{
+//			bird.pos.y -=500  * bird.speed * GetFrameTime();
+//			
+//
+//
+//#ifdef _DEBUG
+//			cout << "y:" << bird.pos.y << endl;
+//#endif
+//		}
+//		else if (bird.pos.y < GetScreenHeight() - 50)
+//		{
+//			bird.pos.y += 1 * bird.speed * GetFrameTime();
+//
+//#ifdef _DEBUG
+//			cout << "y:" << bird.pos.y << endl;
+//#endif	
+//		}	
+//	/*}*/
+//}
 
 void UpdateBird(Bird& bird)
 {
